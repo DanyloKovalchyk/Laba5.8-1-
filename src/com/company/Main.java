cb5058ab2634b5e0283a1f7d5a8d43babb00314f
DanyloKovalchyk <101935665+DanyloKovalchyk@users.noreply.github.com>
2022-03-24 12:24:06 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Завдання 1
+        int n = 60;
+
+        if (n > 50 && n < 100)
+            System.out.print("Завдання 1: Число " +n+ " належить промажку(50, 100)");
+
+        else System.out.print("Завдання 1: Число " +n+ " не належить проміжку(50, 100)");
+    }
+}
